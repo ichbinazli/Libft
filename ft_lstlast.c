fb76da0364d914aaf503809a4b560b0929be585d
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-01-18 00:33:15 +0300

    Update ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -20,3 +20,27 @@ t_list	*ft_lstlast(t_list *lst)
 		lst = lst->next;
 	return (lst);
 }
+/*
+ int		main(void)
+ {
+	char	str1[] = "first element";
+	char	str2[] = "last element";
+
+	t_list	*elem1;
+	t_list	*elem2;
+	t_list	*last_elem;
+
+	if(!(elem1 = malloc(sizeof(t_list))))
+		return (0);
+	if(!(elem2 = malloc(sizeof(t_list))))
+		return (0);
+
+	elem1->next = elem2;
+	elem2->next = NULL;
+
+	elem1->content = (void *)str1;
+	elem2->content = (void *)str2;
+	last_elem = ft_lstlast(elem1);
+	printf("%s", (char *)last_elem->content);
+}
+*/
